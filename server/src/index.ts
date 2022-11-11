519d2cc8f16ca4fc494cc0e25c1c52395c09e8c1
Denis <dionisie.birca@isa.utm.md>
2022-11-11 16:15:12 +0200

    fix static folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,25 @@
 require('dotenv').config();
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import fileUpload from 'express-fileupload'
-import path from "path";
+import fileUpload from 'express-fileupload';
 
 import sequelize from './db';
 import models from '../models/models';
 import router from '../routes/index';
 import ErrorHandlingMiddleware from '../middleware/ErrorHandlingMiddleware';
 
-
-const app: Application = express();
 const port = parseInt(process.env.PORT ?? '3000', 10);
+const app: Application = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('./static'));
+app.use(fileUpload());
 app.use('/api', router);
-app.use(fileUpload({}));
 
 //Error check
 app.use(ErrorHandlingMiddleware);
-
 
 const start = async () => {
     try {
